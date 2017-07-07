3d5df05a73e1922bad5bd71a4b5651e2599c42d0
jennadaly <jenna.m.daly@gmail.com>
2017-07-07 18:29:42 -0400

    Initial commit.

---
+++ scripts/fetal-and-infant-mortality---5-year-aggregations-by-county-processing.R
@@ -0,0 +1,10 @@
+library(dplyr)
+library(datapkg)
+
+##################################################################
+#
+# Processing Script for Fetal and Infant Mortality - 5-Year Aggregations by County
+# Created by Jenna Daly
+# On 07/07/2017
+#
+##################################################################
